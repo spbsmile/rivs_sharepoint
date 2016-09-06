7eb7517b78e3a2597e8c899f814fa1cad3c30723
Забиякин Макар Владимирович <M_Zabiyakin@rivs.ru>
2016-09-06 15:32:31 +0300

    add typings libs

--- CustomjsLibs/typings/index.d.ts
+++ CustomjsLibs/typings/index.d.ts
@@ -1,1 +1,5 @@
+/// <reference path="globals/bootstrap/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/moment-timezone/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
+/// <reference path="globals/sharepoint/index.d.ts" />
