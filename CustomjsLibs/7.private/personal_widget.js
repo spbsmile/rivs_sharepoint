d937008b39a40e5d2fbd9ee5826329c5d4a9741a
Забиякин Макар Владимирович <M_Zabiyakin@rivs.ru>
2016-09-07 11:25:35 +0300

    personal widget remove spservices

--- CustomjsLibs/7.private/personal_widget.js
+++ CustomjsLibs/7.private/personal_widget.js
@@ -1,26 +1,47 @@
 $(document).ready(function () {
 
-    var currentUser = $().SPServices.SPGetCurrentUser();
+    function loadContext() {
+        var context = SP.ClientContext.get_current();
+        getCurrentUser(context, function (user) {
+            WriteUserData(user.get_id());
+        });
+    }
 
-    var thisUsersValues = $().SPServices.SPGetCurrentUser({
-        fieldNames: ["ID", "Title", "EMail", "Picture", "Department", "JobTitle", "WorkPhone", "Name", "SipAddres"],
-        debug: false
-    });
+    function getCurrentUser(context, SubmitCurrentUser) {
+        var currentUser = context.get_web().get_currentUser();
+        context.load(currentUser);
+        context.executeQueryAsync(function (sender, args) {
+            var user = currentUser;
+            user.id = currentUser.get_id();
+            user.login = currentUser.get_loginName();
+            user.name = currentUser.get_title();
+            SubmitCurrentUser(user);
+        },
+            function OnFailure(sender, args) {
+                SubmitCurrentUser(null);
+            });
+    }
 
     var regexStr = /ID=([^"]*?)(?=&)/;
-    var resultRegex = regexStr.exec(decodeURIComponent(window.location.href));    
-    var id = resultRegex ? resultRegex[1] : "";       
-    console.log(id)
+    var resultRegex = regexStr.exec(decodeURIComponent(window.location.href));
+    var id = resultRegex ? resultRegex[1] : "";
+
     if (id) {
+        WriteUserData(id);
+    } else {
+        SP.SOD.executeFunc("sp.js", 'SP.ClientContext', loadContext);
+    }   
+    
+    function WriteUserData(userId) {
         $.ajax({
-            url: "_api/web/getuserbyid(" + id + ")",
+            url: "_api/web/getuserbyid(" + userId + ")",
             method: "GET",
             headers: {
                 "Accept": "application/json;odata=verbose",
                 "X-RequestDigest": $("#__REQUESTDIGEST").val()
             },
             success: function (data) {
-                var datajs = JSON.stringify(data);                
+                var datajs = JSON.stringify(data);
                 var results = data.d;
                 $(".title_person").text(results.Title);
                 $(".email_value").text(results.Email);
@@ -32,35 +53,24 @@ $(document).ready(function () {
                         "Accept": "application/json;odata=verbose",
                         "X-RequestDigest": $("#__REQUESTDIGEST").val()
                     },
-                    success: function (dataNew) {                        
+                    success: function (dataNew) {
                         var results = dataNew.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results;
 
                         for (var i = 0; i < results.length; i++) {
-                            var result = results[i];                            
+                            var result = results[i];
                             $(".job_title_person_widget").text(result.Cells.results[2].Value);
                             $(".phone_value").text(result.Cells.results[3].Value);
                             $(".department_person_widget").text(result.Cells.results[4].Value);
                             var photo = result.Cells.results[5].Value;
                             photo = photo.replace(/ /g, '%20');
                             photo = photo.replace("_MThumb.jpg", "_LThumb.jpg");
-                            $(".image_person").attr("src", photo);                            
+                            $(".image_person").attr("src", photo);
                         }
                     },
                     error: errorHandler
                 });
             }
         });
-    } else {
-        $(".title_person").text(thisUsersValues.Title);
-        var photo = thisUsersValues.Picture;
-        photo = photo.replace(/ /g, '%20');
-        photo = photo.replace("_MThumb.jpg", "_LThumb.jpg");
-
-        $(".email_value").text(thisUsersValues.EMail);
-        $(".department_person_widget").text(thisUsersValues.Department);
-        $(".job_title_person_widget").text(thisUsersValues.JobTitle);
-        $(".phone_value").text(thisUsersValues.WorkPhone);
-        $(".image_person").attr("src", photo);
     }
 
     function errorHandler(data) {
