7dddb4297ac42a2a01d6a86ea812109058e223b7
Забиякин Макар Владимирович <M_Zabiyakin@rivs.ru>
2016-09-08 15:06:10 +0300

    add redirect claim of support

--- CustomjsLibs/6.support/claims/csrHandlers.js
+++ CustomjsLibs/6.support/claims/csrHandlers.js
@@ -1,78 +1,106 @@
-function clickAcceptTask(itemId, created, authId, discription, priority, category, lookupId, employeeId) {    
-    if (employeeId === "null") {
-        employeeId = currentUserId;
+function clickAcceptTask(itemId, created, employeeId, discription, priority, category, fileId, employeeItId) {
+    // for modal window "выдать заявку"
+    if (employeeItId === "null") {
+        employeeItId = currentUserId;
     }
 
     var regex = /<br\s*[\/]?>/gi;
     var dist = discription.replace(regex, "\n");
 
-    var authorClaimId = authId === "null" ? null : authId;
-    var itemData = {
-        "__metadata": {
-            "type": "SP.Data.List1ListItem",
-            "DateCreate": "",
-            "TimeCreate": "",
-            "Author0": "",
-            "Discription": "",
-            "Priority": "",
-            "Category": "",
-            "AttachFileNew": "",
-            "timegettask": "",
-            "employeeIt": ""
-        },
+    employeeId = employeeId === "null" ? null : employeeId;
+    var itemData = null;
+    if (fileId && fileId != null && fileId != "null") {
+        itemData = {
+            "__metadata": {
+                "type": "SP.Data.List1ListItem",
+                "DateCreate": "",
+                "TimeCreate": "",
+                "Author0": "",
+                "Discription": "",
+                "Priority": "",
+                "Category": "",
+                "AttachFileNew": "",
+                "timegettask": "",
+                "employeeIt": ""
+            },
+            "DateCreate": moment().format("L"),
+            "TimeCreate": moment(created, 'DD-MM-YYYY HH:mm').format("HH:mm"),
+            "Author0Id": employeeId,
+            "Discription": dist,
+            "Priority": priority,
+            "Category": category,
+            "AttachFileNewId": fileId,
+            "timegettask": moment().format("L") + "\n" + moment().format("HH:mm"),
+            "employeeItId": employeeItId
+        };
+    } else {
+        itemData = {
+            "__metadata": {
+                "type": "SP.Data.List1ListItem",
+                "DateCreate": "",
+                "TimeCreate": "",
+                "Author0": "",
+                "Discription": "",
+                "Priority": "",
+                "Category": "",
+                "timegettask": "",
+                "employeeIt": ""
+            },
+            "DateCreate": moment().format("L"),
+            "TimeCreate": moment(created, 'DD-MM-YYYY HH:mm').format("HH:mm"),
+            "Author0Id": employeeId,
+            "Discription": dist,
+            "Priority": priority,
+            "Category": category,
+            "timegettask": moment().format("L") + "\n" + moment().format("HH:mm"),
+            "employeeItId": employeeItId
+        };
+    }
 
-        "DateCreate": moment().format("L"),
-        "TimeCreate": moment(created, 'DD-MM-YYYY HH:mm').format("HH:mm"),
-        "Author0Id": authorClaimId,
-        "Discription": dist,
-        "Priority": priority,
-        "Category": category,
-        "AttachFileNewId": lookupId,
-        "timegettask": moment().format("L") + "\n" + moment().format("HH:mm"),
-        "employeeItId": employeeId
-    };
     addItem(settings().listId_acceptedClaims, itemData);
     if (itemId) {
         removeItem(itemId, settings().listId_newClaims);
         $("#loaderClaimGive").hide();
-        document.location.reload();
     }
 }
 
-function clickCloseTask(itemId, d, time, category, urgently, authId, discription, timegettask, commentItJob) {
+function clickCloseTask(itemId, d, time, category, urgently, employeeId, discription, timegettask, commentItJob, fileId, isRedirect) {
     removeItem(itemId, settings().listId_acceptedClaims);
+    if (isRedirect) {
+        var userTargetId = $("#userRedirectTarget").val().split(";")[0];
+        clickAcceptTask(null, moment(), employeeId, discription, urgently, category, fileId, userTargetId);
+    } else {
+        var regex = /<br\s*[\/]?>/gi;
+        var dist = discription.replace(regex, "\n");
 
-    var regex = /<br\s*[\/]?>/gi;
-    var dist = discription.replace(regex, "\n");
-
-    var itemData = {
-        "__metadata": {
-            "type": "SP.Data.List2ListItem",
-            "Date": "",
-            "Time": "",
-            "Author0": "",
-            "Discription": "",
-            "DateAccept": "",
-            "DateResolved": "",
-            "category": "",
-            "Score": "",
-            "urgently": "",
-            "CommEmpIT": "",
-            "CommAuthor": ""
-        },
-        "Date": d,
-        "Time": time,
-        "Author0Id": authId,
-        "Discription": dist,
-        "category": category,
-        "urgently": urgently,
-        "DateAccept": moment(timegettask, 'L').format("L") + "\n" + moment(timegettask, "DD-MM-YYYY HH:mm").format("HH:mm"),
-        "DateResolved": moment().format("L") + "\n" + moment().format("HH:mm"),
-
-        "Score": "Нет оценки",
-        "CommEmpIT": commentItJob,
-        "CommAuthor": "Нет комментария"
-    };
-    addItem(settings().listId_resolvedClaims, itemData);
+        var itemData = {
+            "__metadata": {
+                "type": "SP.Data.List2ListItem",
+                "Date": "",
+                "Time": "",
+                "Author0": "",
+                "Discription": "",
+                "DateAccept": "",
+                "DateResolved": "",
+                "category": "",
+                "Score": "",
+                "urgently": "",
+                "CommEmpIT": "",
+                "CommAuthor": ""
+            },
+            "Date": d,
+            "Time": time,
+            "Author0Id": employeeId,
+            "Discription": dist,
+            "category": category,
+            "urgently": urgently,
+            "DateAccept": moment(timegettask, 'L').format("L") + "\n" + moment(timegettask, "DD-MM-YYYY HH:mm").format("HH:mm"),
+            "DateResolved": moment().format("L") + "\n" + moment().format("HH:mm"),
+            "Score": " ",
+            "CommEmpIT": commentItJob,
+            "CommAuthor": " "
+        };
+        addItem(settings().listId_resolvedClaims, itemData);
+    }
 }
 
--- CustomjsLibs/6.support/claims/modalDialogHandlers.js
+++ CustomjsLibs/6.support/claims/modalDialogHandlers.js
@@ -18,7 +18,7 @@ function clickDialogGetOut() {
                         if ($("#getFile").get(0).files.length === 0) {
                             clickAcceptTask(null, moment(new Date()).format(), userSourceId, $("#discriptionModal").val(), $("#urgencyModalClaim option:selected").text(),
                                 $("#categoryModalClaim option:selected").text(), null, employeId);
-                        } else {                            
+                        } else {
                             sendClaimWithFile(function (itemId) {
                                 clickAcceptTask(null, moment(new Date()).format(), userSourceId, $("#discriptionModal").val(), $("#urgencyModalClaim option:selected").text(),
                                     $("#categoryModalClaim option:selected").text(), itemId, employeId);
@@ -35,24 +35,66 @@ function clickDialogGetOut() {
     });
 }
 
-function clickDialogCommentClosestClaim(itemId, d, time, category, urgently, authId, discription, timegettask) {
+var isRedirect = false;
+
+$(document).ready(function () {
+    $("input[name='userRedirectTarget']").pickSPUser(
+        {
+            onPickUser: function (personObj) {
+                $("#userRedirectTarget").next().find(".pt-pickSPUser-input").hide();
+            },
+            onRemoveUser: function ($input, $ui, personObj) {
+                $("#userRedirectTarget").next().find(".pt-pickSPUser-input").show();
+            },
+            filterSuggestions: function (suggestions) {
+                var newSuggestions = [];
+                var prevEmail = null;
+                $.each(suggestions, function (i, userInfo) {
+                    if (prevEmail === userInfo.email) return true;
+                    prevEmail = userInfo.email;
+                    newSuggestions.push(userInfo);
+                });
+                return newSuggestions;
+            }
+        }
+    );
+    $(".userRedirectClaim").hide();
+});
+
+$(document).on('change', '#selectRedirectState', function () {
+    if ($("#selectRedirectState").val() === "0") {
+        $(".commentContainerModal").show();
+        isRedirect = false;
+        $(".userRedirectClaim").hide();
+    } else {
+        $(".commentContainerModal").hide();
+        isRedirect = true;
+        $(".userRedirectClaim").show();
+    }
+});
+
+function clickDialogCommentClosestClaim(itemId, d, time, category, urgently, authId, discription, timegettask, fileId) {
     $(function () {
         $("#dialog-form-comment-claim").dialog({
             buttons: [
                 {
-                    text: "Закрыть Заявку",
+                    text: "Отправить",
                     click: function () {
                         $("#loaderClaimOut").show();
-                        clickCloseTask(itemId, d, time, category, urgently, authId, discription, timegettask, $("#commentModal").val());
+                        if (isRedirect) {
+                            clickCloseTask(itemId, d, time, category, urgently, authId, discription, timegettask, $("#commentModal").val(), fileId, isRedirect);
+                        } else {
+                            clickCloseTask(itemId, d, time, category, urgently, authId, discription, timegettask, $("#commentModal").val(), fileId);
+                        }
+
                     }
                 }
             ],
-            title: 'Комментирование Заявки: ',
+            title: 'Отправить Заявку: ',
             width: 600,
             modal: true,
             resizable: false
-        })
-            ;
+        });
     });
 }
 
