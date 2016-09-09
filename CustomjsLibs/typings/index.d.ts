29dc124a41a23cb86f3949b9a56e9070a66385c6
Забиякин Макар Владимирович <M_Zabiyakin@rivs.ru>
2016-09-09 17:10:04 +0300

    add ts jqueryui and spservice

--- CustomjsLibs/typings/index.d.ts
+++ CustomjsLibs/typings/index.d.ts
@@ -1,5 +1,7 @@
 /// <reference path="globals/bootstrap/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/jqueryui/index.d.ts" />
 /// <reference path="globals/moment-timezone/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
 /// <reference path="globals/sharepoint/index.d.ts" />
+/// <reference path="globals/spservice/jquery.SPServices.d.ts" />
